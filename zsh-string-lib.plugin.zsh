1135bfd3b4b91a62fb3abf171e39d68fe91224ee
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-11-17 09:24:36 +0100

    plugin.zsh: Add standard fpath extending

--- zsh-string-lib.plugin.zsh
+++ zsh-string-lib.plugin.zsh
@@ -3,6 +3,11 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
+if [[ ${zsh_loaded_plugins[-1]} != */zsh-string-lib && -z ${fpath[(r)${0:h}]} ]]
+then
+    fpath+=( "${0:h}" )
+fi
+
 zmodload zsh/system 2>/dev/null
 
 autoload \
