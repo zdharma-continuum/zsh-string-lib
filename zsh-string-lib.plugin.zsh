d2e66a9be0ad9345702bc2a7decf56178e502d90
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-11-17 09:15:40 +0100

    plugin.zsh: Autoload the functions

--- zsh-string-lib.plugin.zsh
+++ zsh-string-lib.plugin.zsh
@@ -1,7 +1,10 @@
 # According to the Zsh Plugin Standard:
 # http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
-
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-# Then ${0:h} to get plugin's directory
+zmodload zsh/system 2>/dev/null
+
+autoload \
+    @str-parse-json \
+    @str-read-all
