4473e7c722fe4b2d6d95fe6387b3dbe4ded4c784
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-11-22 02:48:29 +0100

    all: Add Emacs mode lines

--- zsh-string-lib.lib.zsh
+++ zsh-string-lib.lib.zsh
@@ -1,3 +1,4 @@
+# -*- mode: sh; sh-indentation: 4; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # According to the Zsh Plugin Standard:
 # http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
